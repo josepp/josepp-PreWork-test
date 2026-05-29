2af33990ae0911f631ca5b81287052570594ed52
josepp <1321527+josepp@users.noreply.github.com>
2026-05-29 17:30:57 +0000

    resultado quiz

--- src/app.js
+++ src/app.js
@@ -5,37 +5,37 @@ para ver en la terminal lo que estes imprimiendo en consola usa -> node src/app.
 <------------------------- INICIO SELECCION SIMPLE -------------------------->
 Pregunta 1: ✅
 ¿Cuál es la etiqueta correcta para insertar una imagen en HTML?
-a) <img src="ruta"> 
+a) <img src="ruta"> ✅
 b) <image src="ruta">
 
 
 Pregunta 2: ✅
 ¿Qué etiqueta HTML se utiliza para crear un enlace?
-a) <a href="url">Texto</a> 
+a) <a href="url">Texto</a> ✅
 b) <link href="url">Texto</link>
 
 
 Pregunta 3: ✅
 ¿Cuál de los siguientes selectores selecciona un elemento por su ID?
-a) #elemento 
+a) #elemento ✅
 b) .elemento
 
 
 Pregunta 4: ✅
 ¿Cuál es la propiedad CSS que se utiliza para cambiar el color del texto?
 a) color 
-b) text-color 
+b) text-color ✅
 
 
 Pregunta 5: ✅
 ¿Cuál propiedad se utiliza para convertir un contenedor en un contenedor flex?
-a) display: flex; 
+a) display: flex; ✅
 b) flex: container
 
 
 Pregunta 6: ✅
 ¿Qué valor de flex-direction coloca los elementos en una columna vertical?
-a) column 
+a) column ✅
 b) vertical
 
 
@@ -45,7 +45,7 @@ Pregunta 7: ✅
 a) space-between distribuye los elementos con espacios iguales entre ellos, dejando sin espacio los bordes del contenedor, 
    mientras que space-around agrega espacio igual entre los elementos y en los bordes del contenedor.
 b) space-between coloca los elementos con espacio igual entre ellos, mientras que space-around distribuye el espacio uniformemente 
-   tanto entre los elementos como en los bordes
+   tanto entre los elementos como en los bordes ✅
 
 
 Pregunta 8: ✅
@@ -55,7 +55,7 @@ Que sucede si ejecutamos powerUps.push("Hoja") ?
 
 a) Se muestra un error porque el arreglo no puede modificarse.
 b) Se agrega "Hoja" al inicio del arreglo.
-c) Se agrega "Hoja" al final del arreglo.
+c) Se agrega "Hoja" al final del arreglo.✅
 d) Se elimina "Hoja" del arreglo.
 
 
@@ -75,7 +75,7 @@ const rescatarAPeach = () => {
 };
 rescatarAPeach();
 
-a) "¡Mario rescató a Peach!"
+a) "¡Mario rescató a Peach!"✅
 b) "No puedes rescatar a Peach todavía."
 c) true
 d) "castillo final"
@@ -89,7 +89,7 @@ function brincar() {
 brincar();
 
 A) Muestra un error
-B) Muestra "Mario brincó!"
+B) Muestra "Mario brincó!"✅
 C) No hace nada
 D) Declara una variable
 
@@ -99,7 +99,7 @@ Pregunta 11: ✅
 
 A) Filtra elementos
 B) Modifica el arreglo original
-C) Devuelve un nuevo arreglo transformado
+C) Devuelve un nuevo arreglo transformado✅
 D) Busca un elemento
 
 
@@ -109,7 +109,7 @@ const base = { nombre: "Toad", rol: "Soporte" };
 const copia = { ...base, nivel: 5 };
 
 A) Borra las propiedades
-B) Copia el objeto base
+B) Copia el objeto base✅ (y añade propiedad nivel: 5)
 C) Suma dos objetos
 D) Crea un array
 
@@ -119,7 +119,7 @@ Pregunta 13: ✅
 const { nombre } = { nombre: "Peach", rol: "Líder" };
 console.log(nombre);
 A) undefined
-B) Peach
+B) Peach✅
 C) rol
 D) Error
 
@@ -131,31 +131,68 @@ D) Error
 
 //----------------------- PARTE PRACTICA -----------------------------
 
+//Pregunta 14:
+// Bowser ha dejado trampas en el inventario
+// Usando el siguiente arreglo:
+let inventario = ["hongo", "flor", "trampa", "estrella", "trampa", "pluma"];
+//Objetivo: Ayuda a Mario a preparar su mochila para la misión final. Realiza los siguientes pasos (haz un console.log después de cada uno):
 
-  //Pregunta 14:
-  // Bowser ha dejado trampas en el inventario
-  // Usando el siguiente arreglo:
-  let inventario = ["hongo", "flor", "trampa", "estrella", "trampa", "pluma"];
-  //Objetivo: Ayuda a Mario a preparar su mochila para la misión final. Realiza los siguientes pasos (haz un console.log después de cada uno):
+// a) Elimina todos los elementos que sean "trampa" (usa un método que filtre el arreglo).
+//    almacena el arreglo filtrado en una variable llamada inventarioSinTrampa (NO MODIFIQUES el arreglo original).
+// b) Agrega "super estrella" al final del inventarioSinTrampas. (debes modificar el arreglo inventarioSinTrampas)
+// c) Recorre el inventarioSinTrampas y convierte todos los nombres en mayúsculas (usa un método que transforme), para convertir un string en mayusculas puedes usar string.toUpperCase()
+//    ejemplo "hongo".toUpperCase()  ---> "HONGO"
+//    el resultado almacenalo en una variable llamada inventarioEnMayusculas. (no modifiques el arregloSinTrampas)
+// d) Usa desestructuración para extraer los dos primeros elementos del inventarioEnMayusculas y guardarlos en variables llamadas primerItem y segundoItem.
 
-  // a) Elimina todos los elementos que sean "trampa" (usa un método que filtre el arreglo). 
-  //    almacena el arreglo filtrado en una variable llamada inventarioSinTrampa (NO MODIFIQUES el arreglo original).
-  // b) Agrega "super estrella" al final del inventarioSinTrampas. (debes modificar el arreglo inventarioSinTrampas)
-  // c) Recorre el inventarioSinTrampas y convierte todos los nombres en mayúsculas (usa un método que transforme), para convertir un string en mayusculas puedes usar string.toUpperCase()
-  //    ejemplo "hongo".toUpperCase()  ---> "HONGO"
-  //    el resultado almacenalo en una variable llamada inventarioEnMayusculas. (no modifiques el arregloSinTrampas)
-  // d) Usa desestructuración para extraer los dos primeros elementos del inventarioEnMayusculas y guardarlos en variables llamadas primerItem y segundoItem.
+//write your code here
+// a) Elimina todos los elementos que sean "trampa" (usa un método que filtre el arreglo).
+let inventarioSinTrampas = inventario.filter((palabra) => palabra !== "trampa");
+// b) Agrega "super estrella" al final del inventarioSinTrampas. (debes modificar el arreglo inventarioSinTrampas)
+inventarioSinTrampas.push("super estrella");
 
-  //write your code here
-  
-  
+// c) Recorre el inventarioSinTrampas y convierte todos los nombres en mayúsculas (usa un método que transforme), para convertir un string en mayusculas puedes usar string.toUpperCase()
+let inventarioEnMayusculas = inventarioSinTrampas.map(function(palabra) {
+  return palabra.toUpperCase();
+});
 
-  // Pregunta 15:
-  // Usando el mismo arreglo de inventario anterior (el original, no los modificados). 
-  // Usa un bucle for o for...of para recorrer el inventario e imprime mensajes personalizados para cada objeto
-  // Si el objeto es "trampa", imprime: "¡Cuidado! Hay una trampa"
-  // Si el objeto es "estrella", imprime: "¡Una estrella! Usar en caso de emergencia"
-  // Para cualquier otro objeto, imprime: "Mario tiene un(a) [nombre del objeto]."
+// d) Usa desestructuración para extraer los dos primeros elementos del inventarioEnMayusculas y guardarlos en variables llamadas primerItem y segundoItem.
+let [primerItem, segundoItem] = inventarioEnMayusculas;
+//console.log(primerItem)
+//console.log(segundoItem)
 
+// Pregunta 15:
+// Usando el mismo arreglo de inventario anterior (el original, no los modificados).
+// Usa un bucle for o for...of para recorrer el inventario e imprime mensajes personalizados para cada objeto
+// Si el objeto es "trampa", imprime: "¡Cuidado! Hay una trampa"
+// Si el objeto es "estrella", imprime: "¡Una estrella! Usar en caso de emergencia"
+// Para cualquier otro objeto, imprime: "Mario tiene un(a) [nombre del objeto]."
+//  let inventario = ["hongo", "flor", "trampa", "estrella", "trampa", "pluma"];
 
-  //write your code here
+for (let cosa of inventario) {
+  let cosa_cmp = cosa.toLowerCase();
+  //version switch
+  switch (cosa_cmp) {
+    case "trampa":
+      console.log("¡Cuidado! Hay una trampa");
+      break;
+    case "estrella":
+      console.log("¡Una estrella! Usar en caso de emergencia");
+      break;
+    default:
+      console.log("Mario tiene un(a) " + cosa + ".");
+      break;
+  }
+
+  //version if else
+  /*
+    if(cosa_cmp == 'trampa'){
+      console.log("¡Cuidado! Hay una trampa");        
+    }else if(cosa_cmp == 'estrella'){
+      console.log("¡Una estrella! Usar en caso de emergencia");
+    }else{
+      console.log("Mario tiene un(a) " + cosa + ".")
+    }*/
+}
+
+//write your code here
